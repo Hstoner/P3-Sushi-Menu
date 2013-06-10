252462851521fb4c590079615b17878300c6144f
Hstoner <hilarystoner@Hilarys-MacBook-Pro.local>
2013-06-10 03:50:08 -0700

    P3-Sushi-Menu
    Finished

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+
+
+$('#spiral').tooltip('hide')
+
+$('#big').tooltip('hide')
+
+$('#sea').tooltip('hide')
+
+$('#banzai').tooltip('hide')
+
+$('#scallop').tooltip('hide')
+
+$('#tuna').tooltip('hide')
+
+$('#maki').tooltip('hide')
+
+$('#roll').tooltip('hide')
